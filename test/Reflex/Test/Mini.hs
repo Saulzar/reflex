99fffaa149358bb1547b0a8a8b4afebb1e45a222
Oliver Batchelor <saulzar@gmail.com>
2015-11-04 18:02:27 +1300

    Coincidences test

--- test/Reflex/Test/Mini.hs
+++ test/Reflex/Test/Mini.hs
@@ -12,6 +12,8 @@ import Data.Foldable
 
 import Control.Monad
 import Data.List
+import Data.Maybe
+import Data.Monoid
 
 runEventFrames ::  EventHandle a -> [[Trigger]] -> IO [Maybe a]
 runEventFrames handle frames = traverse (flip fireEventsAndRead (readEvent handle)) frames
@@ -19,17 +21,42 @@ runEventFrames handle frames = traverse (flip fireEventsAndRead (readEvent handl
 chainMerge :: Int -> Event [Int] -> Event [Int]
 chainMerge depth e = iterate (fmap (fmap (+1)) . leftmost . pure) e !! depth
 
-testSwitching :: Int -> Int -> IO [[Maybe [Int]]]
-testSwitching depth size = do
+setupSwitching :: Int -> Int -> IO ([Event [Int]], [Trigger], [Event (Event [Int])], [Trigger], Int -> [Trigger])
+setupSwitching depth size = do
   (inputs, fireInputs) <- unzip <$> for [1..size] (const newEventWithFire)
   (switchEvents, fireSwitches) <- unzip <$> for [1..size] (const newEventWithFire)
   (switched, fireSwitched) <- newEventWithFire
-
 
   let inputTriggers = fireSwitched [10] : zipWith ($) fireInputs (pure <$> [1..])
       resetTriggers = zipWith ($) fireSwitches inputs
       triggerSwitch i = [(fireSwitches !! i) (chainMerge depth switched)]
 
+  return (inputs, inputTriggers, switchEvents, resetTriggers, triggerSwitch)
+
+
+
+testCoincidence :: Int -> Int -> IO [[[Int]]]
+testCoincidence depth size = do
+  (inputs, inputTriggers, switchEvents, resetTriggers, triggerSwitch) <- setupSwitching depth size
+
+  let events = zipWith (\e e' -> leftmost [coincidence e, e']) switchEvents inputs
+      result = foldr1 mappend events
+
+  handle <- subscribeEvent result
+  let runInputsWith triggers = fireEventsAndRead (triggers <> inputTriggers) (readEvent handle)
+
+      test1 i = triggerSwitch (i - 1)
+      test2 i = triggerSwitch (size - i)
+      test3 i = concatMap triggerSwitch [0..i-1]
+
+
+  for [test1, test2, test3] $ \test -> do
+    fmap catMaybes <$> for [1..size] $ \i -> runInputsWith (test i)
+
+
+testSwitching :: Int -> Int -> IO [[[Int]]]
+testSwitching depth size = do
+  (inputs, inputTriggers, switchEvents, resetTriggers, triggerSwitch) <- setupSwitching depth size
 
   holds <- runHostFrame $ zipWithM hold inputs switchEvents
   let result = foldr1 mappend (switch <$> holds)
@@ -45,12 +72,15 @@ testSwitching depth size = do
 
   for [test1, test2, test3] $ \test -> do
     fireEvents resetTriggers
-    for [1..size] $ \i -> test i >> runInputs
+    fmap catMaybes <$> for [1..size] $ \i -> test i >> runInputs
 
 
-main = for [1..5] $ \depth ->
-  testSwitching depth 5 >>= print
+main = do
+  for [0..4] $ \depth -> do
+    testSwitching depth 4 >>= print
 
-
+  putStrLn ""
+  for [0..4] $ \depth -> do
+    testCoincidence depth 4 >>= print
 
 
