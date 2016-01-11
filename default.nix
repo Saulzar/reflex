3dd88cf252d2933a6162548a882803746e3ecc6c
Oliver Batchelor <saulzar@gmail.com>
2016-01-12 02:11:54 +1300

    Update nix file

--- default.nix
+++ default.nix
@@ -1,17 +1,24 @@
-{ mkDerivation, dependent-map, dependent-sum
-, mtl, ref-tf, semigroups, these, MemoTrie, exception-transformers
-, haskell-src-exts, haskell-src-meta
+{ mkDerivation, base, containers, deepseq, dependent-map
+, dependent-sum, dlist, exception-transformers, haskell-src-exts
+, haskell-src-meta, MemoTrie, mtl, primitive, ref-tf, semigroups
+, split, stdenv, syb, template-haskell, these, transformers
+, transformers-compat
 }:
 mkDerivation {
   pname = "reflex";
-  version = "0.3.2";
-  src = builtins.filterSource (path: type: baseNameOf path != ".git") ./.;
-  buildDepends = [
-    dependent-map dependent-sum mtl ref-tf semigroups these exception-transformers
-    haskell-src-exts haskell-src-meta
+  version = "0.4.0";
+  src = ./.;
+  libraryHaskellDepends = [
+    base containers deepseq dependent-map dependent-sum dlist
+    exception-transformers haskell-src-exts haskell-src-meta mtl
+    primitive ref-tf semigroups syb template-haskell these transformers
+    transformers-compat
   ];
-  testDepends = [
-    MemoTrie
+  testHaskellDepends = [
+    base containers dependent-map dependent-sum MemoTrie mtl ref-tf
+    split
   ];
-  license = null;
+  homepage = "https://github.com/ryantrinkle/reflex";
+  description = "Higher-order Functional Reactive Programming";
+  license = stdenv.lib.licenses.bsd3;
 }
